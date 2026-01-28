39cc7ab3c5a45337da292f2c871eb5238ba4e363
rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>
2026-01-28 20:55:07 +0900

    chore: add Pluto.jl notebook for Gradient Descent chapter
    Signed-off-by: rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>

---
+++ src/Chapter4 - Gradient Descent - Intro to Neural Learning.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a09ed848-fc3f-11f0-9326-eb498920348a
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═a09ed848-fc3f-11f0-9326-eb498920348a
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
