cc35a22f4de1594bcc72da4e42b1e671c5af38e0
rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>
2026-01-30 17:27:09 +0900

    feat: implement multi-input neural network with LinearAlgebra dependency
    Signed-off-by: rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>

--- src/Chapter5 - Generalizing Gradient Descent - Learning Multiple Weights at a Time.jl
+++ src/Chapter5 - Generalizing Gradient Descent - Learning Multiple Weights at a Time.jl
@@ -9,8 +9,16 @@ md"""
 # Gradient Descent Learning with Multiple Inputs
 """
 
-# ╔═╡ ee97fb2c-a612-4591-989f-1097cee41cf6
+# ╔═╡ df5a2868-4486-469a-bcdd-d9fca23b9585
+import LinearAlgebra
 
+# ╔═╡ bb4d9e65-8cc5-4d31-b80a-5df1b8bf9663
+w_sum::Function = LinearAlgebra.dot
+
+# ╔═╡ 732caae4-75b7-4c8f-abb6-058527fedb71
+function neural_network(input::Vector{R}, weight::Vector{R})::R where R<:Real
+	return w_sum(input, weight) # predict
+end
 
 # ╔═╡ 8742b3e1-d64d-40e3-b2fc-cb1f31fb66aa
 md"""
@@ -47,6 +55,7 @@ md"""
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
+LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -55,14 +64,42 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.12.4"
 manifest_format = "2.0"
-project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+project_hash = "f352ceee806168c8ae38887a01d7bae6ca62470b"
 
-[deps]
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+version = "1.11.0"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.3.0+1"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+version = "1.11.0"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+version = "1.12.0"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.29+0"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.15.0+0"
 """
 
 # ╔═╡ Cell order:
 # ╟─0d41a6b0-fdb0-11f0-906e-6f448a888760
-# ╠═ee97fb2c-a612-4591-989f-1097cee41cf6
+# ╠═df5a2868-4486-469a-bcdd-d9fca23b9585
+# ╠═bb4d9e65-8cc5-4d31-b80a-5df1b8bf9663
+# ╠═732caae4-75b7-4c8f-abb6-058527fedb71
 # ╟─8742b3e1-d64d-40e3-b2fc-cb1f31fb66aa
 # ╠═3c56a7b9-fabd-4e4e-817b-d83ff31a40f1
 # ╟─a746572b-14cf-42f8-bc05-6b34f44d8a6b
