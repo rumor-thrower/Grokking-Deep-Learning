dc01916577855dc2f3539229fccb2570e46777fb
rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>
2026-01-30 16:49:25 +0900

    chore: add Chapter 5 notebook for gradient descent with multiple weights
    Signed-off-by: rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>

---
+++ src/Chapter5 - Generalizing Gradient Descent - Learning Multiple Weights at a Time.jl
@@ -0,0 +1,75 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0d41a6b0-fdb0-11f0-906e-6f448a888760
+md"""
+# Gradient Descent Learning with Multiple Inputs
+"""
+
+# ╔═╡ ee97fb2c-a612-4591-989f-1097cee41cf6
+
+
+# ╔═╡ 8742b3e1-d64d-40e3-b2fc-cb1f31fb66aa
+md"""
+# Let's Watch Several Steps of Learning
+"""
+
+# ╔═╡ 3c56a7b9-fabd-4e4e-817b-d83ff31a40f1
+
+
+# ╔═╡ a746572b-14cf-42f8-bc05-6b34f44d8a6b
+md"""
+# Freezing One Weight - What Does It Do?
+"""
+
+# ╔═╡ ca06a804-15db-4ce6-927e-1b158ff7c9e0
+
+
+# ╔═╡ 93ec1387-4a5b-4c88-81ec-e374b22f15cd
+md"""
+# Gradient Descent Learning with Multiple Outputs
+"""
+
+# ╔═╡ ea7fe6ff-81f4-4bd4-bf1a-120fe9053dcb
+
+
+# ╔═╡ 04f8b253-3847-4578-b9cf-7bdc9c67f015
+md"""
+# Gradient Descent with Multiple Inputs & Outputs
+"""
+
+# ╔═╡ b4d2afab-531d-4241-b9bd-a779318c96f3
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─0d41a6b0-fdb0-11f0-906e-6f448a888760
+# ╠═ee97fb2c-a612-4591-989f-1097cee41cf6
+# ╟─8742b3e1-d64d-40e3-b2fc-cb1f31fb66aa
+# ╠═3c56a7b9-fabd-4e4e-817b-d83ff31a40f1
+# ╟─a746572b-14cf-42f8-bc05-6b34f44d8a6b
+# ╠═ca06a804-15db-4ce6-927e-1b158ff7c9e0
+# ╟─93ec1387-4a5b-4c88-81ec-e374b22f15cd
+# ╠═ea7fe6ff-81f4-4bd4-bf1a-120fe9053dcb
+# ╟─04f8b253-3847-4578-b9cf-7bdc9c67f015
+# ╠═b4d2afab-531d-4241-b9bd-a779318c96f3
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
