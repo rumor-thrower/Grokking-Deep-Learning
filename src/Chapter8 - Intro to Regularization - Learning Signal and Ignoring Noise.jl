304bdbedfc5cb58e09a7ea6cfd96eb3661473a8a
rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>
2026-02-06 16:46:46 +0900

    chore: add Chapter 8 notebook on Regularization concepts
    Signed-off-by: rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>

---
+++ src/Chapter8 - Intro to Regularization - Learning Signal and Ignoring Noise.jl
@@ -0,0 +1,71 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ cb6e73d0-032f-11f1-bb3a-73e95d8e819e
+md"""
+# 3 Layer Network on MNIST
+"""
+
+# ╔═╡ f6f2fe48-dfe1-49ba-81ee-fcb0d7f048fd
+
+
+# ╔═╡ 48bf9b99-36fe-4b93-93be-7915a7f95f6c
+
+
+# ╔═╡ 8824f8c9-9ff6-459c-af5f-877519a54748
+
+
+# ╔═╡ bc60ca95-08c7-4bba-a1e1-bbffe07c7267
+
+
+# ╔═╡ f247e919-d416-43d6-953c-afc72721dec6
+md"""
+# Dropout In Code
+"""
+
+# ╔═╡ 86b4e330-bb06-4204-ba5e-74384f79f66a
+
+
+# ╔═╡ ded3e4ba-af36-4444-9b39-c04781adca03
+
+
+# ╔═╡ 1b446766-2d05-4d31-a893-a1fbd5d3217b
+md"""
+# Batch Gradient Descent
+"""
+
+# ╔═╡ af43b3b2-af48-4172-ba28-b81b064c58c3
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─cb6e73d0-032f-11f1-bb3a-73e95d8e819e
+# ╠═f6f2fe48-dfe1-49ba-81ee-fcb0d7f048fd
+# ╠═48bf9b99-36fe-4b93-93be-7915a7f95f6c
+# ╠═8824f8c9-9ff6-459c-af5f-877519a54748
+# ╠═bc60ca95-08c7-4bba-a1e1-bbffe07c7267
+# ╟─f247e919-d416-43d6-953c-afc72721dec6
+# ╠═86b4e330-bb06-4204-ba5e-74384f79f66a
+# ╠═ded3e4ba-af36-4444-9b39-c04781adca03
+# ╟─1b446766-2d05-4d31-a893-a1fbd5d3217b
+# ╠═af43b3b2-af48-4172-ba28-b81b064c58c3
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
